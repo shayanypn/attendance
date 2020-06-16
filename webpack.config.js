782239033f3339c89b885c46ccb9c3d82a577dd7
shayanypn <shayanypn@gmail.com>
2020-06-16 20:12:52 +0430

    add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const is_prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.jsx' ),
@@ -25,9 +28,35 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }
+        test: /\.(s*)css$/,
+        use: [
+          'css-hot-loader',
+          (is_prod ? MiniCssExtractPlugin.loader : { loader: 'style-loader' }),
+          {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+          }, {
+              loader: 'postcss-loader',
+              options: {
+                  ident: 'postcss',
+                  plugins: loader => [
+                      require('postcss-import')({ root: loader.resourcePath }),
+                      require('postcss-preset-env')(),
+                      require('postcss-inline-svg'),
+                      require('postcss-svgo'),
+                  ],
+              },
+          }, {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  includePaths: [
+                    path.resolve(__dirname, './src'),
+                  ],
+                },
+              },
+          }],
+        },
     ]
   },
   plugins: [
