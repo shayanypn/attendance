906c4874a9e6ef92b89a87c9dfa220b017375b3d
shayanypn <shayanypn@gmail.com>
2020-06-17 08:56:18 +0430

    initial the first container and component

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  render() {
+    return (
+    	<div>
+            Form would be here
+    	</div>
+    );
+  }
+}
+
+export default Form;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Home from './containers/Home/index.jsx';
 import './styles/index.scss';
 
 class Index extends Component {
   render() {
-    return ('Simple attendance app');
+    return (<Home />);
   }
 }
 
